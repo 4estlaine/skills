605cb0d7693899a23070c91112b8152eb432566a
Forrest Laine <forrest.laine@berkeley.edu>
2016-12-11 21:12:58 -0800

    Files

--- levels.js
+++ levels.js
@@ -1,24 +1,22 @@
-var level0 = [['blue', 'blue', 'blue'],];
-var level1 = [['blue', 'blue', 'blue'],
+var levelA0 = [['blue', 'blue', 'blue'],];
+var levelA1 = [['blue', 'blue', 'blue'],
               ['red', 'skip', 'red']];
-var level2 = [['blue', 'blue', 'blue', 'blue', 'blue'],
+var levelA2 = [['blue', 'blue', 'blue', 'blue', 'blue'],
               ['red', 'skip', 'skip', 'skip', 'red']];
-var level3 = [['blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue'],
+var levelA3 = [['blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue'],
               ['red', 'skip', 'skip', 'skip', 'skip', 'skip', 'skip', 'red']];
-var level4 = [['purple', 'purple', 'purple'],
+var levelA4 = [['purple', 'purple', 'purple'],
               ['green', 'skip', 'green']];
-var level5 = [['blue', 'green', 'blue'],
+var levelA5 = [['blue', 'green', 'blue'],
               ['purple', 'red', 'red']];
-var level6 = [['blue', 'blue', 'blue', 'blue'],
+var levelA6 = [['blue', 'blue', 'blue', 'blue'],
               ['red', 'red', 'skip', 'red']];
-var level7 = [['orange', 'blue', 'blue'],
+var levelA7 = [['orange', 'blue', 'blue'],
               ['orange', 'red', 'red']];
-var level8 = [['green', 'green', 'purple'],
+var levelA8 = [['green', 'green', 'purple'],
               ['purple', 'red', 'purple']];
 
-
-
-var levelSetA = [level0, level1, level2, level3, level4, level5, level6, level7];
+var levelSetA = [levelA0, levelA1, levelA2, levelA3, levelA4, levelA5, levelA6, levelA7];
 
 var levelSets = [levelSetA];
 
